639e5f50c369fe393479e57a7cecd7997073ea59
mimisco <mimisco4life@gmail.com>
2026-06-05 18:10:26 +0100

    feat: landing page at root, app at /app

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { "@": "/src" } }
+  resolve: { alias: { "@": "/src" } },
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./app.html",
+      }
+    }
+  }
 });
